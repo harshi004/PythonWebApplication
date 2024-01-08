19643d03ffcc7b2bfb5d2cfad6f90be5756be359
harshi004 <145111179+harshi004@users.noreply.github.com>
2024-01-08 13:54:00 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,7 +1,7 @@
 import streamlit as st
 st.set_page_config(page_title='Dogs')
 st.header("Type of Dogs")
-col1,col2,col3=st.columns(3)
+col1,col2,col3,col4=st.columns(4)
 with col1:
   st.subheader("Pomeranian")
   st.image("https://www.dailypaws.com/thmb/MGYnlOlOgW-14ZBI0ouXKJuQWeE=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/pomeranian-white-puppy-921029690-2000-3ca2fdb56d144450a05c6990efdfd40e.jpg",caption="Pomeranian",width=300,use_column_width=True)
@@ -12,6 +12,12 @@ with col2:
   st.write("Shih Tzu means “little lion,” but there’s nothing fierce about this dog breed.")
 with col3:
   st.subheader("Havanese")
-  st.image("https://media4.giphy.com/media/arw1fJJ1F7067yop8m/200w.gif?cid=6c09b952ixs04w5e9z11uk7eopr5ewy2innnspbsqx4txcsi&ep=v1_gifs_search&rid=200w.gif&ct=g",caption="Havanese",width=300,use_column_width=True)
+  st.image("https://media4.giphy.com/media/S8rf64OiYYuCpESD9V/giphy.gif?cid=6c09b952c0k4hvsy9sg98zuzw9atnnbgxjdt3opdo9tanzoq&ep=v1_internal_gif_by_id&rid=giphy.gif&ct=g",caption="Havanese",width=300,use_column_width=True)
   st.write("The Havanese is a charming small dog breed with a long, silky coat, known for its playful demeanor, intelligence, and affectionate nature.")
+with col4:
+  st.subheader("Nandu Chintu")
+  st.video("https://youtu.be/Dmfh85UC3Cc?si=2GnCjjGXir2wTaLh")
+  st.write("Nandu Chintu cute couple") 
+
+ 
 

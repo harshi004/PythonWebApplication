cb93ea1f8444740db4b56a4b8d9966a2b3365ee2
harshi004 <145111179+harshi004@users.noreply.github.com>
2024-01-08 12:16:33 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -4,9 +4,9 @@ st.header("Type of Dogs")
 col1,col2,=st.columns(2)
 with col1:
   st.subheader("Pomeranian")
-  st.image("https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F754423375072546533%2F&psig=AOvVaw1PHKaSoGiZ5Cl-oA3wy-CO&ust=1704782417236000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCPDnh8iXzYMDFQAAAAAdAAAAABAY",caption="Pomeranian",width=300,use_column_width=True)
+  st.image("https://i.pinimg.com/736x/d5/c1/3a/d5c13a12e7af47674c253a2b4dab14d8.jpg",caption="Pomeranian",width=300,use_column_width=True)
   st.write("The Pomeranian is a tiny dog")
 with col2:
   st.subheader("Shih Tzu")
-  st.image("https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.dogster.com%2Flifestyle%2Ffacts-about-shih-tzu&psig=AOvVaw3W4Cdu0k-C9JMIrHlsAi4m&ust=1704782265993000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCOC5iv6WzYMDFQAAAAAdAAAAABAY",caption="Shih Tzu",width=300,use_column_width=True)
+  st.image("https://dogtime.com/wp-content/uploads/sites/12/2011/01/GettyImages-178920540-e1689348859523.jpg?w=1024",caption="Shih Tzu",width=300,use_column_width=True)
   st.write("Shih Tzu means “little lion,” but there’s nothing fierce about this dog breed.")

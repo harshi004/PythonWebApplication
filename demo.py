8d208c4e2fba04793f86a6aa620b1cda88bcf0bd
harshi004 <145111179+harshi004@users.noreply.github.com>
2024-01-08 12:00:33 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -3,7 +3,7 @@ st.set_page_config(page_title='Dogs')
 st.header("Type of Dogs")
 col1,col2,=st.columns(2)
 with col1:
-  st.subheder("Pomeranian")
+  st.subheader("Pomeranian")
   st.image("./Dog1.jpg",caption="Pomeranian",width=300,use_column_width=True)
   st.write("The Pomeranian is a tiny dog")
 with col2:

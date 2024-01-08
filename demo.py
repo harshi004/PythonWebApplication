606b00c18384ac2d37aff18d0640c197316c1155
harshi004 <145111179+harshi004@users.noreply.github.com>
2024-01-08 11:51:39 +0530

    Create demo.py

---
+++ demo.py
@@ -0,0 +1,12 @@
+import streamlit as st
+st.set_page_config(Page_title='Dogs')
+st.header("Type of Dogs")
+col1,col2,=st.columns(2)
+with col1:
+  st.subheder("Pomeranian")
+  st.image("./Dog1.jpg",caption="Pomeranian",width=300,use_column_width=True)
+  st.write("The Pomeranian is a tiny dog")
+with col2:
+  st.subheader("Shih Tzu")
+  st.image("./Shih Tzu.jpg",caption="Shih Tzu",width=300,use_column_width=True)
+  st.write("Shih Tzu means “little lion,” but there’s nothing fierce about this dog breed.")

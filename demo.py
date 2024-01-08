5f8546e8b6226f552db336d8ac2fe19f8cc9af38
harshi004 <145111179+harshi004@users.noreply.github.com>
2024-01-08 12:04:45 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -4,9 +4,9 @@ st.header("Type of Dogs")
 col1,col2,=st.columns(2)
 with col1:
   st.subheader("Pomeranian")
-  st.image(""C:\Users\harsh\OneDrive\Desktop\Dog1.jpg"",caption="Pomeranian",width=300,use_column_width=True)
+  st.image("C:\Users\harsh\OneDrive\Desktop\Dog1.jpg",caption="Pomeranian",width=300,use_column_width=True)
   st.write("The Pomeranian is a tiny dog")
 with col2:
   st.subheader("Shih Tzu")
-  st.image(""C:\Users\harsh\OneDrive\Desktop\Shih Tzu.jpg",caption="Shih Tzu",width=300,use_column_width=True)
+  st.image("C:\Users\harsh\OneDrive\Desktop\Shih Tzu.jpg",caption="Shih Tzu",width=300,use_column_width=True)
   st.write("Shih Tzu means “little lion,” but there’s nothing fierce about this dog breed.")

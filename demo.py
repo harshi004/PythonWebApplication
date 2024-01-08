35291a5afd6579af559ee3f14b9a2730b590be94
harshi004 <145111179+harshi004@users.noreply.github.com>
2024-01-08 14:16:04 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,23 +1,17 @@
 import streamlit as st
 st.set_page_config(page_title='Dogs')
-st.header("Type of Dogs")
-col1,col2,col3,col4=st.columns(4)
+st.header("Crazy Things")
+col1,col2,col3=st.columns(3)
 with col1:
-  st.subheader("Pomeranian")
-  st.image("https://www.dailypaws.com/thmb/MGYnlOlOgW-14ZBI0ouXKJuQWeE=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/pomeranian-white-puppy-921029690-2000-3ca2fdb56d144450a05c6990efdfd40e.jpg",caption="Pomeranian",width=300,use_column_width=True)
-  st.write("Pomeranian dogs are small in stature but big in personality. Alert and confident, Pomeranians have a beautiful coat and charm to match.")
+  st.subheader("DOREMON")
+  st.image("https://m.media-amazon.com/images/M/MV5BNTkzNjJhYjQtNjU0Yy00Y2M3LWI2ZDgtNDRhZmNlNDFjMjQ5XkEyXkFqcGdeQXVyODk2ODI3MTU@._V1_FMjpg_UX1000_.jpg",caption="Pomeranian",width=300,use_column_width=True)
+  st.write("Doremon is a cartoon character,which helps to Nobita")
 with col2:
-  st.subheader("Shih Tzu")
-  st.image("https://dogtime.com/wp-content/uploads/sites/12/2011/01/GettyImages-178920540-e1689348859523.jpg?w=1024",caption="Shih Tzu",width=300,use_column_width=True)
-  st.write("Shih Tzu means “little lion,” but there’s nothing fierce about this dog breed.")
+  st.subheader("DOREMON Gif")
+  st.image("https://i.pinimg.com/originals/59/30/74/593074c302700c41ae6fdfeca3d51563.gif",caption="Shih Tzu",width=300,use_column_width=True)
+  st.write("Kiddo Doremon.")
 with col3:
-  st.subheader("Havanese")
-  st.image("https://media4.giphy.com/media/S8rf64OiYYuCpESD9V/giphy.gif?cid=6c09b952c0k4hvsy9sg98zuzw9atnnbgxjdt3opdo9tanzoq&ep=v1_internal_gif_by_id&rid=giphy.gif&ct=g",caption="Havanese",width=300,use_column_width=True)
-  st.write("The Havanese is a charming small dog breed with a long, silky coat, known for its playful demeanor, intelligence, and affectionate nature.")
-with col4:
   st.subheader("Doremon")
   st.video("https://youtu.be/J4E1JH2nprs?si=WIE3ynga20IDwzCC")
-  st.write("Doraemon is the name of a robot cat that came from the future to help a boy named Nobita Nobi.") 
-
- 
+  st.write("Doraemon is the name of a robot cat that came from the future to help a boy named Nobita Nobi.")   
 
